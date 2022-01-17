09ed620d1c84fa445797ef8774b1e7047acad8a1
Hayley McHugh <hayleym522@gmail.com>
2022-01-16 16:31:46 -0800

    fixed route

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-require(".Develop/routing/route-html")(app);
+require("./Develop/routing/route-html")(app);
 require("./Develop/routing/route-api")(app);
 
 app.use("/public", express.static('./Develop/public'));
